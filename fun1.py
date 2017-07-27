69fa11bf489cdbd1e8cac2ef00b2c80b2a167950
tamara19-meet <tamara19@meet.mit.edu>
2017-07-27 12:53:05 +0200

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,15 @@
+
+def add_numbers(start, end):
+    c = 0
+    for number in range (start , end + 1):
+        #print(number)
+        c = c + number
+    return(c)
+
+
+answer = add_numbers(333,777)
+print(answer)
+
+
+
+
